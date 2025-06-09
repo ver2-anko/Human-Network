bad5adc8de5f9a99ac9502a996c8084287e9c74d
anko_chan <ayurx78@gmail.com>
2025-06-09 23:44:27 +0900

    デザイン調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-//import React from 'react';
-import NetWork from './features/Network/NetWork';
+import NetWork from './components/NetWork';
+import { Box, Grid, ThemeProvider, Toolbar } from '@mui/material'
+import { AppBar, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button';
+import { Fragment } from 'react/jsx-runtime';
+import { AccountCircleRounded } from '@mui/icons-material';
+import {theme} from './assets/theme'
 
 const App = () => {
   return (
-    <div>
-      <h1>Sample</h1>
-      <NetWork />
-    </div>
+    <Fragment>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position='static' color='primary'>
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} fontFamily={'Segoe UI Symbol'} color='#24140e'>
+                Human Network
+              </Typography>
+              <AccountCircleRounded fontSize='large' />
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Grid container sx={{ mt: 10 }} justifyContent={'center'}>
+          <NetWork></NetWork>
+        </Grid>
+      </ThemeProvider>
+    </Fragment>
   );
 };
 
